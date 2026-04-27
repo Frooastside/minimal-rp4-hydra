8465bb25c9e10b5f60585cc5ff7c0b7b959aeba6
Simon Benezan <simon@benezan.de>
2026-04-27 22:28:44 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -10,8 +10,13 @@
       nixos-hardware,
     }:
     rec {
-      images = {
+      images = rec {
         rp4 = self.nixosConfigurations.rp4.config.system.build.image;
+        rp4_2 = nixpkgs.legacyPackages.aarch64-linux.runCommand "rp4-image-hydra" { } ''
+          mkdir -p $out/nix-support
+          cp ${rp4}/image.raw $out/rp4-image.raw
+          echo "file raw $out/rp4-image.raw" > $out/nix-support/hydra-build-products
+        '';
       };
       nixosConfigurations.rp4 = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
@@ -24,7 +29,7 @@
       packages.x86_64-linux.default = images.rp4;
       packages.aarch64-linux.default = images.rp4;
       hydraJobs = {
-        rp4-image = images.rp4;
+        rp4-image = images.rp4_2;
       };
     };
 }
