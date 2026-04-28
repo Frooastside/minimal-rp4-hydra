6ac792483b005850be7e519c612a3eb69ca04ac6
Simon <47527944+Frooastside@users.noreply.github.com>
2026-04-28 10:08:46 +0200

    Update default user password in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,7 @@
   networking.hostName = "rp4-nixos";
   users = {
     users.default = {
-      password = "default123456789";
+      password = "default1234789";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUlTyMHf9lIjlXuilVWZT0I9BqZUNN2gsy5U+D3SZ+Z simon.benezan@h-net.com"
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIO6NPut/WhzhJNQNPlshqVHhsODamuekMPfKit9h5XVvAAAAG3NzaDpzaW1vbi5iZW5lemFuQGgtbmV0LmNvbQ== simon.benezan@h-net.com"
